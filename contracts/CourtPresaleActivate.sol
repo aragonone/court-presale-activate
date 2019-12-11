41ed89a33b4e1bf9e28e3b97adb4e48e53c9b829
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2019-12-11 14:05:03 -0300

    chore: update court contracts to v1.1.0-rc.1

--- contracts/CourtPresaleActivate.sol
+++ contracts/CourtPresaleActivate.sol
@@ -18,9 +18,11 @@ contract CourtPresaleActivate is IsContract{
 
     bytes32 internal constant ACTIVATE_DATA = keccak256("activate(uint256)");
 
-    ERC20 bondedToken;
-    ERC900 registry;
-    IPresale presale;
+    ERC20 public bondedToken;
+    ERC900 public registry;
+    IPresale public presale;
+
+    event TokensActivated(address indexed token, address indexed sender, address indexed activator, uint256 amount);
 
     constructor(ERC20 _bondedToken, ERC900 _registry, IPresale _presale) public {
         require(isContract(address(_bondedToken)), ERROR_TOKEN_NOT_CONTRACT);
@@ -33,22 +35,22 @@ contract CourtPresaleActivate is IsContract{
     }
 
     /**
-     * @dev User must first approve `_amount` of bondedToken to this contract
-     */
+    * @notice Buy and activate `@tokenAmount(self.bondedToken(): address, _amount, false)` tokens for sender
+    */
     function buyAndActivate(uint256 _amount) external {
         require(_amount > 0, ERROR_ZERO_AMOUNT);
-        // sign activation
-        // TODO
 
         // buy in presale
+        // TODO: we should approve and call this function
         presale.contribute(msg.sender, _amount);
-
         uint256 bondedTokensObtained = presale.contributionToTokens(_amount);
-        // activate in registry
-        emit LogBAA(address(bondedToken), msg.sender, address(this), _amount);
+
+        // approve bonded tokens to registry
         require(bondedToken.safeTransferFrom(msg.sender, address(this), bondedTokensObtained), ERROR_TOKEN_TRANSFER_FAILED);
         bondedToken.approve(address(registry), bondedTokensObtained);
+
+        // activate in registry
         registry.stakeFor(msg.sender, bondedTokensObtained, abi.encodePacked(ACTIVATE_DATA));
+        emit TokensActivated(address(bondedToken), msg.sender, address(this), _amount);
     }
-    event LogBAA(address bt, address sender, address th, uint amount);
 }
